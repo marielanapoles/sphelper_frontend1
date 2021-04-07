3ae8aa262053d0098f74315b21dfa521724c8789
Mariela Napoles <mariela.napoles@student.tafesa.edu.au>
2021-04-07 11:18:32 +0930

    study plan manager modified

---
+++ sphelper_try1/Models/DataManager/Student_StudyplanManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace sphelper_try1.Models.DataManager
+{
+    public class Student_StudyplanManager
+    {
+        public static student FindStudentById(string studentId)
+        {
+            using (SpsDatabaseEntities context = new SpsDatabaseEntities())
+            {
+                var query = context.students.Where(x => x.StudentID == studentId);
+                student student1 = new student()
+                {
+                    StudentID = query.First().StudentID,
+                    GivenName = query.First().GivenName,
+                    LastName = query.First().LastName,
+                    EmailAddress = query.First().EmailAddress
+                };
+                return student1;
+            }
+        }
+
+        public static qualification FindQualificationByStudentId(string studentId)
+        {
+
+            using (SpsDatabaseEntities context = new SpsDatabaseEntities())
+            {
+                var query = from student_studyplan in context.student_studyplan
+                            join qualification in context.qualifications
+                            on student_studyplan.QualCode equals qualification.QualCode
+                            where student_studyplan.StudentID == studentId
+                            select new
+                            {
+                                qualification.QualCode,
+                                qualification.QualName,
+                                qualification.NationalQualCode,
+                                qualification.TafeQualCode
+
+                            };
+                qualification student_qualification = new qualification()
+                {
+                    QualCode = query.First().QualCode,
+                    QualName = query.First().QualName,
+                    NationalQualCode = query.First().NationalQualCode,
+                    TafeQualCode = query.First().TafeQualCode
+                };
+
+                return student_qualification;
+            }
+        }
+
+        public static string FindStudyplanByQualificaitonCode(string qualCode)
+        {
+            using (SpsDatabaseEntities context = new SpsDatabaseEntities())
+            {
+                var query = context.studyplan_qualification.Where(x => x.QualCode == qualCode && x.Priority == 1);
+                string studyplan = query.First().StudyPlanCode;
+
+                return studyplan;
+            }
+        }
+
+        public static List<TableItems> GetAllSubjectByStudyplan(string qualification)
+        {
+            using (SpsDatabaseEntities context = new SpsDatabaseEntities())
+            {
+                var query = from studyplan_subject in context.studyplan_subject
+                             join subject subject in context.subjects
+                             on studyplan_subject.SubjectCode equals subject.SubjectCode
+                             where studyplan_subject.StudyPlanCode == qualification //qualification string
+                             orderby studyplan_subject.TimingSemester, studyplan_subject.TimingSemesterTerm
+                             select new TableItems {
+                                 SubjectCode =  subject.SubjectCode,
+                                 SubjectTitle = subject.SubjectDescription,
+                                 SubjectDescription = subject.SubjectLongDescription,
+                                 Semester = studyplan_subject.TimingSemester.ToString(),
+                                 TermSemester = studyplan_subject.TimingSemesterTerm.ToString()
+                             };
+                return query.ToList();
+
+            }
+        }
+    }
+}
---
+++ sphelper_try1/ViewModel/SelectStudyplanSubjectVM.cs
@@ -0,0 +1,36 @@
+﻿using sphelper_try1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sphelper_try1.ViewModel
+{
+    public class SelectStudyplanSubjectVM
+    {
+        //get Student Name
+        public string StudentName { get; set; }
+
+        //get Qualification
+        public string Qualification { get; set; }
+
+        //get nationalCode
+        public string NationalCode { get; set; }
+
+        //get tafecode
+        public string TafeCode { get; set; }
+
+
+        //get semester and term timing 
+        public List<string> Timing { get; set; }
+
+        //get subjects
+        public List<SubjectCheckItems> CheckItems { get; set; }
+
+        public SelectStudyplanSubjectVM()
+        {
+            CheckItems = new List<SubjectCheckItems>();
+            Timing = new List<string>();
+        }
+    }
+}
